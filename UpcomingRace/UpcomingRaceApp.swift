1589b5ce38230124049beb893172d8f9a0af422c
Bibin Pulickal <bpulickal@woolworths.com.au>
2024-12-16 13:06:14 +1030

    Initial Commit

---
+++ UpcomingRace/UpcomingRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingRaceApp.swift
+//  UpcomingRace
+//
+//  Created by Bibin Jacob Pulickal on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UpcomingRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpcomingRaceTests/UpcomingRaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingRaceTests.swift
+//  UpcomingRaceTests
+//
+//  Created by Bibin Jacob Pulickal on 16/12/2024.
+//
+
+import Testing
+@testable import UpcomingRace
+
+struct UpcomingRaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
